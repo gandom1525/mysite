153d52f89c20472c8683e192c16bad4063eb9c6f
maryam gandom <gandommaryam51@gmail.com>
2025-10-14 21:39:02 +0330

    created first app
    website and three views

---
+++ website/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+
+def home_page(request):
+    return HttpResponse("home_page")
+
+def about_page(request):
+    return HttpResponse("about_page")
+
+def countent_page(request):
+    return HttpResponse("countent_page")
+
+
+# def http_test(request):
+#     return HttpResponse("hi maryam ")
+
+# def json_test(request):
+#     return JsonResponse({'name':"ali"})
+
+# Create your views here.
