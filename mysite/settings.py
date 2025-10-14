1889f638bc8606a9242042b88acf4af01c6bc691
maryam gandom <gandommaryam51@gmail.com>
2025-10-14 22:39:59 +0330

    add render
    hast

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5z4m$%bdum+kqtqi_c)m=v_wa^ys$&xhgv&3vnbtu!0a2jj9l!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
