b6baad47f58a1c247758cb6e497045a4016a9623
maryam gandom <gandommaryam51@gmail.com>
2025-10-15 23:19:34 +0330

    creat rander html file
    rander

--- website/views.py
+++ website/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
+
 def home_page(request):
-    return HttpResponse("home_page")
+    return render(request,'index.html')
 
 def about_page(request):
-    return HttpResponse("about_page")
+    return render(request,'about.html')
 
 def countent_page(request):
-    return HttpResponse("countent_page")
+    return render(request,'countent.html')
+
+
+# def home_page(request):
+#     return HttpResponse("home_page")
+
+# def about_page(request):
+#     return HttpResponse("about_page")
+
+# def countent_page(request):
+#     return HttpResponse("countent_page")
 
 
 # def http_test(request):
