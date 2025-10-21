8a84cfebd76ac79bf3cb016a43b22ad84423b795
maryam gandom <gandommaryam51@gmail.com>
2025-10-21 20:39:35 +0330

    adddd

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,14 +115,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static' / 'statics'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "statics"
+    BASE_DIR / 'statics',
 ]
 
 
