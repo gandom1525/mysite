318976564531e5e877f64c125d5bff81c3a9932a
maryam gandom <gandommaryam51@gmail.com>
2026-02-04 01:34:08 +0330

    blog single post

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ app_name='blog'
 
 urlpatterns = [
     path('',blog_page,name='index'),
-    path('single',single_page,name='single'),
-    path('post-<int:pid>',test,name='test')
+    path('<int:pid>',single_page,name='single'),
+    # path('post-<int:pid>',test,name='test')
     # path('test',test,name='test')
 
 
--- blog/views.py
+++ blog/views.py
@@ -6,22 +6,18 @@ def blog_page(request):
     context ={'posts':posts}
     return render(request,'blog/blog-home.html',context)
    
-def single_page(request):
-    return render(request,'blog/blog-single.html')
-
-def test(request,pid):
-    # posts =Post.objects.all()
-    # posts =Post.objects.filter(status=1)
-    # post = Post.objects.get(id=pid)
+def single_page(request,pid):
     post = get_object_or_404(Post,pk=pid)
     context ={'post':post}
-    return render (request,"test.html",context)
+    return render(request,'blog/blog-single.html',context)
 
-
-
-
-
-
+# def test(request,pid):
+#     # posts =Post.objects.all()
+#     # posts =Post.objects.filter(status=1)
+#     # post = Post.objects.get(id=pid)
+#     post = get_object_or_404(Post,pk=pid)
+#     context ={'post':post}
+#     return render (request,"test.html",context)
 
 
 
